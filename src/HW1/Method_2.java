0078506e36ebe228963b4c90ee50abd3e1955271
Kim <98963260+oanhhkim@users.noreply.github.com>
2023-01-12 13:52:24 +0700

    update

---
+++ src/HW1/Method_2.java
@@ -0,0 +1,33 @@
+package HW1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Method_2 {
+    /*
+     * Viết chương trình tìm tất cả các số chia hết cho 7 nhưng không phải bội số của 5,
+     * nằm trong đoạn 10 và 200 (tính cả 10 và 200). Các số thu được sẽ được in thành chuỗi trên một dòng,
+     * cách nhau bằng dấu phẩy
+     */
+
+    public static void main(String[] args) {
+        System.out.println(display());
+    }
+
+    public static List<Integer> checkNum(){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 10; i<=200; i++){
+            if(i%7==0 && i%5!=0){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+    public static String display(){
+        String str = new String(checkNum().toString());
+        String str2= str.replace("["," ");
+        String str3 = str2.replace("]"," ");
+        return str3;
+    }
+
+}
